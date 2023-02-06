3ac34035ca98278d211f2d246bbaf2529c43badc
GRuoro <chrisgachuhi@gmail.com>
2023-02-06 11:13:40 +0300

    email js update

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useRef }  from 'react'
 
 import Navbar from '../Components/Navbar';
 import Button from 'react-bootstrap/Button'
@@ -6,7 +6,32 @@ import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 
+import emailjs from '@emailjs/browser';
+
 function Contact() {
+    const emailRef = useRef()
+    const nameRef = useRef();
+    const messageRef = useRef();
+
+    function submit() {
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const comment = messageRef.current.value;
+
+        const templateParams = {
+            from_name: name,
+            reply_to: email,
+            message: comment
+        };
+    
+        emailjs.send('service_70o8tvl', 'template_p52oy2p', templateParams,'RRkye2MWgbsfhUjO2')
+            .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+            console.log('FAILED...', error);
+            });
+    }
+
   return (
     <div className = "contactPage">
         <Navbar/>
@@ -21,18 +46,18 @@ function Contact() {
         <div className="contactForm">
             <div className="contactDetails">
                 <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
-                    <Form.Control type="text" placeholder="name" style={{ height: '60px' }}/>
+                      <Form.Control ref={nameRef} type="text" placeholder="name" style={{ height: '60px' }}/>
                 </FloatingLabel>
                 
                 <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
-                    <Form.Control type="email" placeholder="name@example.com" style={{ height: '60px' }}/>
+                      <Form.Control ref={emailRef}type="email" placeholder="name@example.com" style={{ height: '60px' }}/>
                 </FloatingLabel>
                 
                 <FloatingLabel controlId="floatingTextarea2" label="Comments">
-                    <Form.Control as="textarea" placeholder="Leave a comment here" style={{ height: '200px' }}/>
+                      <Form.Control as="textarea" ref={messageRef} placeholder="Leave a comment here" style={{ height: '200px' }}/>
                 </FloatingLabel>
                 
-                <Button>SUBMIT</Button>
+                <Button onClick={submit}>SUBMIT</Button>
             </div>
           </div>
 
